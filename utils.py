217580d1b94ef90b44fcda2add345fc6be84cd5e
sxyazi <sxyazi@gmail.com>
2019-02-28 23:17:25 +0800

    add config options

--- utils.py
+++ utils.py
@@ -3,10 +3,6 @@ import hashlib
 from datetime import datetime
 
 
-def item_hash(d):
-    return hashlib.md5(pickle.dumps(d)).hexdigest()
-
-
 def path_format(path):
     while '//' in path:
         path = path.replace('//', '/')
