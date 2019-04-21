2e8d5525a055d4ce6758ceddf547f33ad6e931a1
malaohu <laohu_ma@163.com>
2019-04-21 18:44:46 +0800

    start app use 0.0.0.0

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ app = Flask(__name__)
 app.register_blueprint(bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80', debug=False)
+    app.run(host='0.0.0.0', port='5000', debug=False)
