d2be3126baf86cbdd531257e35d486ad0084a835
malaohu <re_ming.ma@hotmail.com>
2019-04-23 20:13:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ app = Flask(__name__)
 app.register_blueprint(bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=False)
+    app.run(host='0.0.0.0', port='8080', debug=False)
